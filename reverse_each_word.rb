3076d8a26736be9b58ce4f6c9978e0a0cb5745c6
Alex Farmer <farmale10@gmail.com>
2019-08-30 22:09:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 
 def reverse_each_word(string)
-  womp = string.split
-  boop = womp.collect { |string| string.reverse }
-  boop.join(" ")
+  string_to_array = string.split
+  new_map = string_to_array.collect { |string| string.reverse }
+  new_map.join(" ")
 end
